9c168f2e0f454817cb525515896254774d31f02b
Benny Cheung <daydreamer10191980@gmail.com>
2025-04-09 18:19:34 +0800

    Fix TypeScript error: extend session.user to include 'id'

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -11,7 +11,7 @@ export const authConfig: NextAuthOptions = {
   callbacks: {
   async session({ session, token }) {
     if (session.user) {
-      session.user.id = token.sub
+      session.user.id = token.sub as string
     }
     return session
   },
