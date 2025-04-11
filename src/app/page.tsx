9fca20231073fd7f693325d8f66597778db68f54
Benny Cheung <daydreamer10191980@gmail.com>
2025-04-11 14:16:39 +0800

    add back pb-64, 2nd attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -360,9 +360,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="chat-container relative flex flex-col flex-1 max-h-screen overflow-hidden">
-
-
+      <div className="chat-container relative flex flex-col flex-1 max-h-screen overflow-y-auto">
 
         <div className="flex-1 overflow-y-auto pt-14 md:pt-16" ref={messagesRef}>
           <div id="chat-messages" className="p-4 space-y-2 pb-64">
