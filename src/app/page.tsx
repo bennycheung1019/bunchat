c2dd6f7574de63f37b00fdfc93ee64f45f60f76e
Benny Cheung <daydreamer10191980@gmail.com>
2025-04-11 14:12:28 +0800

    add back pb-64

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -365,7 +365,7 @@ export default function Home() {
 
 
         <div className="flex-1 overflow-y-auto pt-14 md:pt-16" ref={messagesRef}>
-          <div id="chat-messages" className="p-4 space-y-2">
+          <div id="chat-messages" className="p-4 space-y-2 pb-64">
 
 
             {chatMode === "replyEmail" && (
