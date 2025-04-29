cfab5602a50c31edd57ac14753c5fa905fc6960a
Benny Cheung <daydreamer10191980@gmail.com>
2025-04-29 17:17:51 +0800

    #9

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,16 +2,21 @@ import { ReactNode } from "react";
 import LocaleClientLayout from "@/components/LocaleClientLayout";
 import { LoadUserSettingsOnStart } from "@/components/LoadUserSettingsOnStart";
 
-type LocaleLayoutProps = {
+export default function LocaleLayout({
+    children,
+    params,
+}: {
     children: ReactNode;
     params: { locale: string };
-};
-
-export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
+}) {
     return (
-        <LocaleClientLayout locale={params.locale}>
-            <LoadUserSettingsOnStart />
-            {children}
-        </LocaleClientLayout>
+        <html lang={params.locale}>
+            <body>
+                <LocaleClientLayout locale={params.locale}>
+                    <LoadUserSettingsOnStart />
+                    {children}
+                </LocaleClientLayout>
+            </body>
+        </html>
     );
 }
