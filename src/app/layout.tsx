2209720e20acf7c03dd9a84dce39a52c652b0143
Benny Cheung <daydreamer10191980@gmail.com>
2025-05-20 16:07:46 +0800

    fix clickable email #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <html lang="en">
             <head>
                 <meta name="color-scheme" content="light" />
+                <meta name="format-detection" content="telephone=no, email=no" />
             </head>
             <body>{children}</body>
         </html>
